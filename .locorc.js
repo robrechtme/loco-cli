0d253ab11d1695a5579a8b5b550d990aae8bacdc
robrechtme <robrechtmeersman@hotmail.com>
2022-08-27 13:18:29 +0200

    chore: replace dummy access key
    I accidentally pushed my access key, don't worry it is already revoked :ok_hand:.

--- .locorc.js
+++ .locorc.js
@@ -1,14 +1,14 @@
 /** @type {import('loco-cli/types').Config} */
 module.exports = {
-  accessKey: 'RGxcxXtH_yEU__3O3nHTr8xrm_RYJ2I70',
-  localesDir: 'public/locales',
+  accessKey: "<access-key>",
+  localesDir: "public/locales",
   namespaces: true,
   push: {
-    'flag-new': 'provisional',
-    'tag-new': process.env.npm_package_version,
-    'delete-absent': false,
+    "flag-new": "provisional",
+    "tag-new": process.env.npm_package_version,
+    "delete-absent": false,
   },
   pull: {
-    fallback: 'en',
+    fallback: "en",
   },
 };
